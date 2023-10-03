d35cda09307ab676b210587a622fe7431dd8c334
kKsk <alonzomax1210@gmail.com>
2023-10-03 23:05:50 +0800

    Edited Layout

--- BandizipBatchOperation/Main_Form.Designer.cs
+++ BandizipBatchOperation/Main_Form.Designer.cs
@@ -35,7 +35,6 @@
             label2 = new Label();
             textBox_UnzipToAddress = new TextBox();
             button_GetUnzipToAddress = new Button();
-            label3 = new Label();
             radioButton_Type_zip = new RadioButton();
             radioButton_Type_7z = new RadioButton();
             radioButton_Type_rar = new RadioButton();
@@ -47,6 +46,8 @@
             checkBox_UnzipToSameNameDir = new CheckBox();
             checkBox_DeleteSrc = new CheckBox();
             button_StartUnzip = new Button();
+            groupBox_radioButton = new GroupBox();
+            groupBox_radioButton.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -78,7 +79,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 77);
+            label2.Location = new Point(12, 62);
             label2.Name = "label2";
             label2.Size = new Size(99, 20);
             label2.TabIndex = 3;
@@ -86,84 +87,72 @@
             // 
             // textBox_UnzipToAddress
             // 
-            textBox_UnzipToAddress.Location = new Point(12, 100);
+            textBox_UnzipToAddress.Location = new Point(12, 85);
             textBox_UnzipToAddress.Name = "textBox_UnzipToAddress";
             textBox_UnzipToAddress.Size = new Size(361, 27);
             textBox_UnzipToAddress.TabIndex = 4;
             // 
             // button_GetUnzipToAddress
             // 
-            button_GetUnzipToAddress.Location = new Point(379, 100);
+            button_GetUnzipToAddress.Location = new Point(379, 85);
             button_GetUnzipToAddress.Name = "button_GetUnzipToAddress";
             button_GetUnzipToAddress.Size = new Size(91, 27);
             button_GetUnzipToAddress.TabIndex = 5;
             button_GetUnzipToAddress.Text = "选取...";
             button_GetUnzipToAddress.UseVisualStyleBackColor = true;
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(12, 149);
-            label3.Name = "label3";
-            label3.Size = new Size(189, 20);
-            label3.TabIndex = 6;
-            label3.Text = "文件格式类型（文件后缀）";
-            // 
             // radioButton_Type_zip
             // 
             radioButton_Type_zip.AutoSize = true;
-            radioButton_Type_zip.Location = new Point(12, 172);
+            radioButton_Type_zip.Location = new Point(6, 26);
             radioButton_Type_zip.Name = "radioButton_Type_zip";
             radioButton_Type_zip.Size = new Size(51, 24);
             radioButton_Type_zip.TabIndex = 7;
-            radioButton_Type_zip.TabStop = true;
             radioButton_Type_zip.Text = "zip";
             radioButton_Type_zip.UseVisualStyleBackColor = true;
             // 
             // radioButton_Type_7z
             // 
             radioButton_Type_7z.AutoSize = true;
-            radioButton_Type_7z.Location = new Point(125, 172);
+            radioButton_Type_7z.Location = new Point(119, 26);
             radioButton_Type_7z.Name = "radioButton_Type_7z";
             radioButton_Type_7z.Size = new Size(46, 24);
             radioButton_Type_7z.TabIndex = 8;
-            radioButton_Type_7z.TabStop = true;
             radioButton_Type_7z.Text = "7z";
             radioButton_Type_7z.UseVisualStyleBackColor = true;
             // 
             // radioButton_Type_rar
             // 
             radioButton_Type_rar.AutoSize = true;
-            radioButton_Type_rar.Location = new Point(69, 172);
+            radioButton_Type_rar.Location = new Point(63, 26);
             radioButton_Type_rar.Name = "radioButton_Type_rar";
             radioButton_Type_rar.Size = new Size(50, 24);
             radioButton_Type_rar.TabIndex = 9;
-            radioButton_Type_rar.TabStop = true;
             radioButton_Type_rar.Text = "rar";
             radioButton_Type_rar.UseVisualStyleBackColor = true;
             // 
             // radioButton_Type_other
             // 
             radioButton_Type_other.AutoSize = true;
-            radioButton_Type_other.Location = new Point(177, 172);
+            radioButton_Type_other.Location = new Point(171, 26);
             radioButton_Type_other.Name = "radioButton_Type_other";
             radioButton_Type_other.Size = new Size(180, 24);
             radioButton_Type_other.TabIndex = 10;
-            radioButton_Type_other.TabStop = true;
             radioButton_Type_other.Text = "其他后缀（右侧填写）";
             radioButton_Type_other.UseVisualStyleBackColor = true;
+            radioButton_Type_other.CheckedChanged += radioButton_Type_other_CheckedChanged;
             // 
             // textBox_Type
             // 
-            textBox_Type.Location = new Point(363, 172);
+            textBox_Type.Location = new Point(357, 23);
             textBox_Type.Name = "textBox_Type";
-            textBox_Type.Size = new Size(107, 27);
+            textBox_Type.Size = new Size(95, 27);
             textBox_Type.TabIndex = 11;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 216);
+            label4.Location = new Point(12, 182);
             label4.Name = "label4";
             label4.Size = new Size(264, 20);
             label4.TabIndex = 12;
@@ -171,7 +160,7 @@
             // 
             // textBox_Unzip_Password
             // 
-            textBox_Unzip_Password.Location = new Point(12, 239);
+            textBox_Unzip_Password.Location = new Point(12, 205);
             textBox_Unzip_Password.Name = "textBox_Unzip_Password";
             textBox_Unzip_Password.Size = new Size(458, 27);
             textBox_Unzip_Password.TabIndex = 13;
@@ -179,7 +168,7 @@
             // checkBox_UnzipToSameDir
             // 
             checkBox_UnzipToSameDir.AutoSize = true;
-            checkBox_UnzipToSameDir.Location = new Point(12, 272);
+            checkBox_UnzipToSameDir.Location = new Point(12, 238);
             checkBox_UnzipToSameDir.Name = "checkBox_UnzipToSameDir";
             checkBox_UnzipToSameDir.Size = new Size(151, 24);
             checkBox_UnzipToSameDir.TabIndex = 14;
@@ -189,7 +178,7 @@
             // checkBox_UnzipToSameNameDir
             // 
             checkBox_UnzipToSameNameDir.AutoSize = true;
-            checkBox_UnzipToSameNameDir.Location = new Point(12, 302);
+            checkBox_UnzipToSameNameDir.Location = new Point(12, 268);
             checkBox_UnzipToSameNameDir.Name = "checkBox_UnzipToSameNameDir";
             checkBox_UnzipToSameNameDir.Size = new Size(166, 24);
             checkBox_UnzipToSameNameDir.TabIndex = 15;
@@ -199,7 +188,7 @@
             // checkBox_DeleteSrc
             // 
             checkBox_DeleteSrc.AutoSize = true;
-            checkBox_DeleteSrc.Location = new Point(12, 332);
+            checkBox_DeleteSrc.Location = new Point(12, 298);
             checkBox_DeleteSrc.Name = "checkBox_DeleteSrc";
             checkBox_DeleteSrc.Size = new Size(151, 24);
             checkBox_DeleteSrc.TabIndex = 16;
@@ -208,30 +197,39 @@
             // 
             // button_StartUnzip
             // 
-            button_StartUnzip.Location = new Point(261, 272);
+            button_StartUnzip.Location = new Point(261, 238);
             button_StartUnzip.Name = "button_StartUnzip";
             button_StartUnzip.Size = new Size(209, 84);
             button_StartUnzip.TabIndex = 17;
             button_StartUnzip.Text = "开始";
             button_StartUnzip.UseVisualStyleBackColor = true;
             // 
+            // groupBox_radioButton
+            // 
+            groupBox_radioButton.Controls.Add(radioButton_Type_zip);
+            groupBox_radioButton.Controls.Add(radioButton_Type_rar);
+            groupBox_radioButton.Controls.Add(radioButton_Type_7z);
+            groupBox_radioButton.Controls.Add(radioButton_Type_other);
+            groupBox_radioButton.Controls.Add(textBox_Type);
+            groupBox_radioButton.Location = new Point(12, 118);
+            groupBox_radioButton.Name = "groupBox_radioButton";
+            groupBox_radioButton.Size = new Size(458, 61);
+            groupBox_radioButton.TabIndex = 18;
+            groupBox_radioButton.TabStop = false;
+            groupBox_radioButton.Text = "文件格式类型（文件后缀）";
+            // 
             // Main_Form
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(482, 368);
+            ClientSize = new Size(482, 333);
+            Controls.Add(groupBox_radioButton);
             Controls.Add(button_StartUnzip);
             Controls.Add(checkBox_DeleteSrc);
             Controls.Add(checkBox_UnzipToSameNameDir);
             Controls.Add(checkBox_UnzipToSameDir);
             Controls.Add(textBox_Unzip_Password);
             Controls.Add(label4);
-            Controls.Add(textBox_Type);
-            Controls.Add(radioButton_Type_other);
-            Controls.Add(radioButton_Type_rar);
-            Controls.Add(radioButton_Type_7z);
-            Controls.Add(radioButton_Type_zip);
-            Controls.Add(label3);
             Controls.Add(button_GetUnzipToAddress);
             Controls.Add(textBox_UnzipToAddress);
             Controls.Add(label2);
@@ -243,6 +241,9 @@
             Name = "Main_Form";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Bandizip文件批量操作";
+            Load += Main_Form_Load;
+            groupBox_radioButton.ResumeLayout(false);
+            groupBox_radioButton.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -255,7 +256,6 @@
         private Label label2;
         private TextBox textBox_UnzipToAddress;
         private Button button_GetUnzipToAddress;
-        private Label label3;
         private RadioButton radioButton_Type_zip;
         private RadioButton radioButton_Type_7z;
         private RadioButton radioButton_Type_rar;
@@ -267,5 +267,6 @@
         private CheckBox checkBox_UnzipToSameNameDir;
         private CheckBox checkBox_DeleteSrc;
         private Button button_StartUnzip;
+        private GroupBox groupBox_radioButton;
     }
 }
--- BandizipBatchOperation/Main_Form.cs
+++ BandizipBatchOperation/Main_Form.cs
@@ -6,5 +6,29 @@ namespace BandizipBatchOperation
         {
             InitializeComponent();
         }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            // 默认选中第一个，但不选中其余
+            radioButton_Type_zip.Checked = true;
+
+            radioButton_Type_rar.Checked = false;
+            radioButton_Type_7z.Checked = false;
+            radioButton_Type_other.Checked = false;
+
+            textBox_Type.Enabled = false;
+        }
+
+        private void radioButton_Type_other_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton_Type_other.Checked == true)
+            {
+                textBox_Type.Enabled = true;
+            }
+            else
+            {
+                textBox_Type.Enabled =false;
+            }
+        }
     }
 }
