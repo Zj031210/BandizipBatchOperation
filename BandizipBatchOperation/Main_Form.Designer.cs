0cd9d750f7cd5c166d5e46c04714c614b64ca150
kKsk <alonzomax1210@gmail.com>
2023-10-03 22:49:57 +0800

    Arrange UI layout and control ID

--- BandizipBatchOperation/Main_Form.Designer.cs
+++ BandizipBatchOperation/Main_Form.Designer.cs
@@ -28,18 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            label1 = new Label();
+            textBox_AddressWaitUnzip = new TextBox();
+            button_GetAddressWaitUnzip = new Button();
+            label2 = new Label();
+            textBox_UnzipToAddress = new TextBox();
+            button_GetUnzipToAddress = new Button();
+            label3 = new Label();
+            radioButton_Type_zip = new RadioButton();
+            radioButton_Type_7z = new RadioButton();
+            radioButton_Type_rar = new RadioButton();
+            radioButton_Type_other = new RadioButton();
+            textBox_Type = new TextBox();
+            label4 = new Label();
+            textBox_Unzip_Password = new TextBox();
+            checkBox_UnzipToSameDir = new CheckBox();
+            checkBox_UnzipToSameNameDir = new CheckBox();
+            checkBox_DeleteSrc = new CheckBox();
+            button_StartUnzip = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 20);
+            label1.TabIndex = 0;
+            label1.Text = "存放待解压文件的文件夹";
+            // 
+            // textBox_AddressWaitUnzip
+            // 
+            textBox_AddressWaitUnzip.Location = new Point(12, 32);
+            textBox_AddressWaitUnzip.Name = "textBox_AddressWaitUnzip";
+            textBox_AddressWaitUnzip.Size = new Size(361, 27);
+            textBox_AddressWaitUnzip.TabIndex = 1;
+            // 
+            // button_GetAddressWaitUnzip
+            // 
+            button_GetAddressWaitUnzip.Location = new Point(379, 32);
+            button_GetAddressWaitUnzip.Name = "button_GetAddressWaitUnzip";
+            button_GetAddressWaitUnzip.Size = new Size(91, 27);
+            button_GetAddressWaitUnzip.TabIndex = 2;
+            button_GetAddressWaitUnzip.Text = "选取...";
+            button_GetAddressWaitUnzip.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 20);
+            label2.TabIndex = 3;
+            label2.Text = "解压至文件夹";
+            // 
+            // textBox_UnzipToAddress
+            // 
+            textBox_UnzipToAddress.Location = new Point(12, 100);
+            textBox_UnzipToAddress.Name = "textBox_UnzipToAddress";
+            textBox_UnzipToAddress.Size = new Size(361, 27);
+            textBox_UnzipToAddress.TabIndex = 4;
+            // 
+            // button_GetUnzipToAddress
+            // 
+            button_GetUnzipToAddress.Location = new Point(379, 100);
+            button_GetUnzipToAddress.Name = "button_GetUnzipToAddress";
+            button_GetUnzipToAddress.Size = new Size(91, 27);
+            button_GetUnzipToAddress.TabIndex = 5;
+            button_GetUnzipToAddress.Text = "选取...";
+            button_GetUnzipToAddress.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(189, 20);
+            label3.TabIndex = 6;
+            label3.Text = "文件格式类型（文件后缀）";
+            // 
+            // radioButton_Type_zip
+            // 
+            radioButton_Type_zip.AutoSize = true;
+            radioButton_Type_zip.Location = new Point(12, 172);
+            radioButton_Type_zip.Name = "radioButton_Type_zip";
+            radioButton_Type_zip.Size = new Size(51, 24);
+            radioButton_Type_zip.TabIndex = 7;
+            radioButton_Type_zip.TabStop = true;
+            radioButton_Type_zip.Text = "zip";
+            radioButton_Type_zip.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Type_7z
+            // 
+            radioButton_Type_7z.AutoSize = true;
+            radioButton_Type_7z.Location = new Point(125, 172);
+            radioButton_Type_7z.Name = "radioButton_Type_7z";
+            radioButton_Type_7z.Size = new Size(46, 24);
+            radioButton_Type_7z.TabIndex = 8;
+            radioButton_Type_7z.TabStop = true;
+            radioButton_Type_7z.Text = "7z";
+            radioButton_Type_7z.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Type_rar
+            // 
+            radioButton_Type_rar.AutoSize = true;
+            radioButton_Type_rar.Location = new Point(69, 172);
+            radioButton_Type_rar.Name = "radioButton_Type_rar";
+            radioButton_Type_rar.Size = new Size(50, 24);
+            radioButton_Type_rar.TabIndex = 9;
+            radioButton_Type_rar.TabStop = true;
+            radioButton_Type_rar.Text = "rar";
+            radioButton_Type_rar.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_Type_other
+            // 
+            radioButton_Type_other.AutoSize = true;
+            radioButton_Type_other.Location = new Point(177, 172);
+            radioButton_Type_other.Name = "radioButton_Type_other";
+            radioButton_Type_other.Size = new Size(180, 24);
+            radioButton_Type_other.TabIndex = 10;
+            radioButton_Type_other.TabStop = true;
+            radioButton_Type_other.Text = "其他后缀（右侧填写）";
+            radioButton_Type_other.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Type
+            // 
+            textBox_Type.Location = new Point(363, 172);
+            textBox_Type.Name = "textBox_Type";
+            textBox_Type.Size = new Size(107, 27);
+            textBox_Type.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 216);
+            label4.Name = "label4";
+            label4.Size = new Size(264, 20);
+            label4.TabIndex = 12;
+            label4.Text = "解压密码（若有则填写，若无则留空）";
+            // 
+            // textBox_Unzip_Password
+            // 
+            textBox_Unzip_Password.Location = new Point(12, 239);
+            textBox_Unzip_Password.Name = "textBox_Unzip_Password";
+            textBox_Unzip_Password.Size = new Size(458, 27);
+            textBox_Unzip_Password.TabIndex = 13;
+            // 
+            // checkBox_UnzipToSameDir
+            // 
+            checkBox_UnzipToSameDir.AutoSize = true;
+            checkBox_UnzipToSameDir.Location = new Point(12, 272);
+            checkBox_UnzipToSameDir.Name = "checkBox_UnzipToSameDir";
+            checkBox_UnzipToSameDir.Size = new Size(151, 24);
+            checkBox_UnzipToSameDir.TabIndex = 14;
+            checkBox_UnzipToSameDir.Text = "解压至同一目录中";
+            checkBox_UnzipToSameDir.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_UnzipToSameNameDir
+            // 
+            checkBox_UnzipToSameNameDir.AutoSize = true;
+            checkBox_UnzipToSameNameDir.Location = new Point(12, 302);
+            checkBox_UnzipToSameNameDir.Name = "checkBox_UnzipToSameNameDir";
+            checkBox_UnzipToSameNameDir.Size = new Size(166, 24);
+            checkBox_UnzipToSameNameDir.TabIndex = 15;
+            checkBox_UnzipToSameNameDir.Text = "解压至同名文件夹中";
+            checkBox_UnzipToSameNameDir.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_DeleteSrc
+            // 
+            checkBox_DeleteSrc.AutoSize = true;
+            checkBox_DeleteSrc.Location = new Point(12, 332);
+            checkBox_DeleteSrc.Name = "checkBox_DeleteSrc";
+            checkBox_DeleteSrc.Size = new Size(151, 24);
+            checkBox_DeleteSrc.TabIndex = 16;
+            checkBox_DeleteSrc.Text = "解压后删除源文件";
+            checkBox_DeleteSrc.UseVisualStyleBackColor = true;
+            // 
+            // button_StartUnzip
+            // 
+            button_StartUnzip.Location = new Point(261, 272);
+            button_StartUnzip.Name = "button_StartUnzip";
+            button_StartUnzip.Size = new Size(209, 84);
+            button_StartUnzip.TabIndex = 17;
+            button_StartUnzip.Text = "开始";
+            button_StartUnzip.UseVisualStyleBackColor = true;
             // 
             // Main_Form
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(482, 653);
+            ClientSize = new Size(482, 368);
+            Controls.Add(button_StartUnzip);
+            Controls.Add(checkBox_DeleteSrc);
+            Controls.Add(checkBox_UnzipToSameNameDir);
+            Controls.Add(checkBox_UnzipToSameDir);
+            Controls.Add(textBox_Unzip_Password);
+            Controls.Add(label4);
+            Controls.Add(textBox_Type);
+            Controls.Add(radioButton_Type_other);
+            Controls.Add(radioButton_Type_rar);
+            Controls.Add(radioButton_Type_7z);
+            Controls.Add(radioButton_Type_zip);
+            Controls.Add(label3);
+            Controls.Add(button_GetUnzipToAddress);
+            Controls.Add(textBox_UnzipToAddress);
+            Controls.Add(label2);
+            Controls.Add(button_GetAddressWaitUnzip);
+            Controls.Add(textBox_AddressWaitUnzip);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Main_Form";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Bandizip文件批量操作";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox_AddressWaitUnzip;
+        private Button button_GetAddressWaitUnzip;
+        private Label label2;
+        private TextBox textBox_UnzipToAddress;
+        private Button button_GetUnzipToAddress;
+        private Label label3;
+        private RadioButton radioButton_Type_zip;
+        private RadioButton radioButton_Type_7z;
+        private RadioButton radioButton_Type_rar;
+        private RadioButton radioButton_Type_other;
+        private TextBox textBox_Type;
+        private Label label4;
+        private TextBox textBox_Unzip_Password;
+        private CheckBox checkBox_UnzipToSameDir;
+        private CheckBox checkBox_UnzipToSameNameDir;
+        private CheckBox checkBox_DeleteSrc;
+        private Button button_StartUnzip;
     }
 }
